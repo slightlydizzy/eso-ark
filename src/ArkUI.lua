a555812e21ce72fb1ca74bb388b3b61a9a812e0d
slightlydizzy <chrishenry.ni@gmail.com>
2016-02-07 01:20:23 -0800

    Update ArkUI.lua
    Multiple fixes.

--- src/ArkUI.lua
+++ src/ArkUI.lua
@@ -1,5 +1,5 @@
 ArkUI = {
-  name = "ArkUI"
+  name = "ArkUI",
   attributes = {}
 }
 
@@ -32,7 +32,7 @@ function ArkUI:OnAttributeUpdate(powerType, current, max, effectiveMax)
 
   self.attributes[powerType].label:SetColor(1, ratio, ratio, 1)
   self.attributes[powerType].label:SetText(current .. " / " .. max .. " (" .. percentage .. "%)")
-	self.attributes[powerType].lastValue = current
+  self.attributes[powerType].lastValue = current
 
   if diff ~= 0 then
     if diff > 0 then
@@ -43,7 +43,7 @@ function ArkUI:OnAttributeUpdate(powerType, current, max, effectiveMax)
       self.attributes[powerType].diffLabel:SetText(diff)
     end
     self.attributes[powerType].diffFade:PlayFromStart()
-	end
+  end
 end
 
 function ArkUI:UpdateReticleOverHealth(current, max)
@@ -53,7 +53,7 @@ function ArkUI:UpdateReticleOverHealth(current, max)
   self.reticleLabel:SetText(current .. " / " .. max .. " (" .. val .. "%)")
 end
 
-function ArkUI:Initialize
+function ArkUI:Initialize()
   self.savedVars = ZO_SavedVars:New("ArkUI_SavedVariables", 1, nil, defaults)
   self.unitName = GetUnitName("player")
 
@@ -66,59 +66,59 @@ function ArkUI:Initialize
     regenLabel = WINDOW_MANAGER:CreateControlFromVirtual(health:GetName() .. "ArkUIRegenLabel", health, "ArkUIAttributeBarLabel"),
     diffLabel = WINDOW_MANAGER:CreateControlFromVirtual(health:GetName() .. "ArkUIDiffLabel", health, "ArkUIAttributeBarLabel"),
     lastValue = 0,
-	}
-	healthTable.label:SetAnchor(BOTTOM, health, TOP, 0, 2)
-	healthTable.regenLabel:SetAnchor(RIGHT, healthTable.label, LEFT, -8, 0)
-	healthTable.regenLabel:SetColor(ZO_NORMAL_TEXT:UnpackRGBA())
-	healthTable.diffLabel:SetAnchor(LEFT, healthTable.label, RIGHT, 8, 0)
-	healthTable.diffFade = ANIMATION_MANAGER:CreateTimelineFromVirtual("ArkUIAttributeBarDiffFade", healthTable.diffLabel)
-	self.attributes[POWERTYPE_HEALTH] = healthTable
+  }
+  healthTable.label:SetAnchor(BOTTOM, health, TOP, 0, 2)
+  healthTable.regenLabel:SetAnchor(RIGHT, healthTable.label, LEFT, -8, 0)
+  healthTable.regenLabel:SetColor(ZO_NORMAL_TEXT:UnpackRGBA())
+  healthTable.diffLabel:SetAnchor(LEFT, healthTable.label, RIGHT, 8, 0)
+  healthTable.diffFade = ANIMATION_MANAGER:CreateTimelineFromVirtual("ArkUIAttributeBarDiffFade", healthTable.diffLabel)
+  self.attributes[POWERTYPE_HEALTH] = healthTable
 	
-	local stamina = GetControl(PLAYER_ATTRIBUTE_BARS.control, "Stamina")
-	local staminaTable = {
-		label = WINDOW_MANAGER:CreateControlFromVirtual(stamina:GetName().."ArkUIAttributeLabel", stamina, "ArkUIAttributeBarLabel"),
-		statIndex = STAT_STAMINA_MAX,
-		statRegenIndex = STAT_STAMINA_REGEN_IDLE,
-		statRegenInCombatIndex = STAT_STAMINA_REGEN_COMBAT,
-		regenLabel = WINDOW_MANAGER:CreateControlFromVirtual(stamina:GetName().."ArkUIRegenLabel", stamina, "ArkUIAttributeBarLabel"),
-		diffLabel = WINDOW_MANAGER:CreateControlFromVirtual(stamina:GetName().."ArkUIDiffLabel", stamina, "ArkUIAttributeBarLabel"),
-		lastVal = 0,
-	}
-	staminaTable.label:SetAnchor(BOTTOMLEFT, stamina, TOPLEFT, 0, 2)
-	staminaTable.regenLabel:SetAnchor(LEFT, staminaTable.label, RIGHT, 8, 0)
-	staminaTable.regenLabel:SetColor(ZO_NORMAL_TEXT:UnpackRGBA())
-	staminaTable.diffLabel:SetAnchor(LEFT, staminaTable.regenLabel, RIGHT, 8, 0)
-	staminaTable.diffFade = ANIMATION_MANAGER:CreateTimelineFromVirtual("ArkUIAttributeBarDiffFade", staminaTable.diffLabel)
-	self.attributes[POWERTYPE_STAMINA] = staminaTable
+  local stamina = GetControl(PLAYER_ATTRIBUTE_BARS.control, "Stamina")
+  local staminaTable = {
+    label = WINDOW_MANAGER:CreateControlFromVirtual(stamina:GetName().."ArkUIAttributeLabel", stamina, "ArkUIAttributeBarLabel"),
+    statIndex = STAT_STAMINA_MAX,
+    statRegenIndex = STAT_STAMINA_REGEN_IDLE,
+    statRegenInCombatIndex = STAT_STAMINA_REGEN_COMBAT,
+    regenLabel = WINDOW_MANAGER:CreateControlFromVirtual(stamina:GetName().."ArkUIRegenLabel", stamina, "ArkUIAttributeBarLabel"),
+    diffLabel = WINDOW_MANAGER:CreateControlFromVirtual(stamina:GetName().."ArkUIDiffLabel", stamina, "ArkUIAttributeBarLabel"),
+    lastVal = 0,
+  }
+  staminaTable.label:SetAnchor(BOTTOMLEFT, stamina, TOPLEFT, 0, 2)
+  staminaTable.regenLabel:SetAnchor(LEFT, staminaTable.label, RIGHT, 8, 0)
+  staminaTable.regenLabel:SetColor(ZO_NORMAL_TEXT:UnpackRGBA())
+  staminaTable.diffLabel:SetAnchor(LEFT, staminaTable.regenLabel, RIGHT, 8, 0)
+  staminaTable.diffFade = ANIMATION_MANAGER:CreateTimelineFromVirtual("ArkUIAttributeBarDiffFade", staminaTable.diffLabel)
+  self.attributes[POWERTYPE_STAMINA] = staminaTable
 	
-	local magicka = GetControl(PLAYER_ATTRIBUTE_BARS.control, "Magicka")
-	local magickaTable = {
-		label = WINDOW_MANAGER:CreateControlFromVirtual(magicka:GetName().."ArkUIAttributeLabel", magicka, "ArkUIAttributeBarLabel"),
-		statIndex = STAT_MAGICKA_MAX,
-		statRegenIndex = STAT_MAGICKA_REGEN_IDLE,
-		statRegenInCombatIndex = STAT_MAGICKA_REGEN_COMBAT,
-		regenLabel = WINDOW_MANAGER:CreateControlFromVirtual(magicka:GetName().."ArkUIRegenLabel", magicka, "ArkUIAttributeBarLabel"),
-		diffLabel = WINDOW_MANAGER:CreateControlFromVirtual(magicka:GetName().."ArkUIDiffLabel", magicka, "ArkUIAttributeBarLabel"),
-		lastVal = 0,
-	}
-	magickaTable.label:SetAnchor(BOTTOMRIGHT, magicka, TOPRIGHT, 0, 2)
-	magickaTable.regenLabel:SetAnchor(RIGHT, magickaTable.label, LEFT, -8, 0)
-	magickaTable.regenLabel:SetColor(ZO_NORMAL_TEXT:UnpackRGBA())
-	magickaTable.diffLabel:SetAnchor(RIGHT, magickaTable.regenLabel, LEFT, -8, 0)
-	magickaTable.diffFade = ANIMATION_MANAGER:CreateTimelineFromVirtual("ArkUIAttributeBarDiffFade", magickaTable.diffLabel)
-	self.attributes[POWERTYPE_MAGICKA] = magickaTable
+  local magicka = GetControl(PLAYER_ATTRIBUTE_BARS.control, "Magicka")
+  local magickaTable = {
+    label = WINDOW_MANAGER:CreateControlFromVirtual(magicka:GetName().."ArkUIAttributeLabel", magicka, "ArkUIAttributeBarLabel"),
+    statIndex = STAT_MAGICKA_MAX,
+    statRegenIndex = STAT_MAGICKA_REGEN_IDLE,
+    statRegenInCombatIndex = STAT_MAGICKA_REGEN_COMBAT,
+    regenLabel = WINDOW_MANAGER:CreateControlFromVirtual(magicka:GetName().."ArkUIRegenLabel", magicka, "ArkUIAttributeBarLabel"),
+    diffLabel = WINDOW_MANAGER:CreateControlFromVirtual(magicka:GetName().."ArkUIDiffLabel", magicka, "ArkUIAttributeBarLabel"),
+    lastVal = 0,
+  }
+  magickaTable.label:SetAnchor(BOTTOMRIGHT, magicka, TOPRIGHT, 0, 2)
+  magickaTable.regenLabel:SetAnchor(RIGHT, magickaTable.label, LEFT, -8, 0)
+  magickaTable.regenLabel:SetColor(ZO_NORMAL_TEXT:UnpackRGBA())
+  magickaTable.diffLabel:SetAnchor(RIGHT, magickaTable.regenLabel, LEFT, -8, 0)
+  magickaTable.diffFade = ANIMATION_MANAGER:CreateTimelineFromVirtual("ArkUIAttributeBarDiffFade", magickaTable.diffLabel)
+  self.attributes[POWERTYPE_MAGICKA] = magickaTable
 
-	self.reticleLabel = WINDOW_MANAGER:CreateControlFromVirtual(
-	    ZO_TargetUnitFramereticleover:GetName() .. "ArkUILabel",
-	    ZO_TargetUnitFramereticleover,
-	    "ArkUIAttributeBarsLabel")
-	self.reticleLabel:SetAnchor(TOP, ZO_TargetUnitFramereticleover, BOTTOM)
+  self.reticleLabel = WINDOW_MANAGER:CreateControlFromVirtual(
+      ZO_TargetUnitFramereticleover:GetName() .. "ArkUILabel",
+      ZO_TargetUnitFramereticleover,
+      "ArkUIAttributeBarLabel")
+  self.reticleLabel:SetAnchor(TOP, ZO_TargetUnitFramereticleover, BOTTOM)
 
-	local _, point, _, relPoint, x, y = ZO_TargetUnitFramereticleoverTextArea:GetAnchor(0)
-	ZO_TargetUnitFramereticleoverTextArea:ClearAnchors()
-	ZO_TargetUnitFramereticleoverTextArea:SetAnchor(point, self.reticleLabel, relPoint, x, y - 6)
+  local _, point, _, relPoint, x, y = ZO_TargetUnitFramereticleoverTextArea:GetAnchor(0)
+  ZO_TargetUnitFramereticleoverTextArea:ClearAnchors()
+  ZO_TargetUnitFramereticleoverTextArea:SetAnchor(point, self.reticleLabel, relPoint, x, y - 6)
 
-	PLAYER_ATTRIBUTE_BARS:ForceShow(true)
+  PLAYER_ATTRIBUTE_BARS:ForceShow(true)
 
   EVENT_MANAGER:RegisterForEvent(self.name, EVENT_PLAYER_ACTIVATED, ArkUI.PlayerActivated)
   EVENT_MANAGER:RegisterForEvent(self.name, EVENT_POWER_UPDATE, ArkUI.PowerUpdate)
